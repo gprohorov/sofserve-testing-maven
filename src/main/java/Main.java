5aa294757c8463e6a144a0dfa06e21b86a64798b
gprohorov <george.prokhorov@external.getneteurope.com>
2023-03-13 19:10:27 +0200

    impl

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*
+  @author   george
+  @project   sofserve-testing-maven
+  @class  Main
+  @version  1.0.0 
+  @since 12.03.23 - 23.04
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    String email = "rt01.abc@ya.u1.u2";
+    System.out.println(MyUtils.checkEmail(email));
+  }
+}
---
+++ src/main/java/MyUtils.java
@@ -0,0 +1,40 @@
+/*
+  @author   george
+  @project   sofserve-testing-maven
+  @class  MyUtils
+  @version  1.0.0 
+  @since 12.03.23 - 21.44
+*/
+
+import com.sun.jdi.CharValue;
+
+import java.util.regex.Pattern;
+
+public class MyUtils {
+    public static boolean checkEmail(String text) {
+        if (!text.contains("@")) {
+            return false;
+        }
+        var arr1 = text.split("@");
+        String username = arr1[0];
+        if (!Character.isLetter(username.charAt(0))) {
+            return false;
+        }
+
+        boolean test1 = Pattern.matches("[A-Za-z0-9\\.]+", username);
+
+        if (!test1) return false;
+
+        var domains = arr1[1].split("\\.");
+        if (domains[0].length() < 2 || domains.length < 2 ) {
+            return false;
+        }
+    for (int i = 0; i < domains.length; i++) {
+        if (!Pattern.matches("[A-Za-z0-9]+", domains[i])) {
+            return false;
+        }
+    }
+
+        return true;
+    }
+}
